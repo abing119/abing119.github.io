5bbb49f10b67880f6325d2750184c054548a8f59
yilong0110 <okyi@vip.qq.com>
2022-05-12 12:31:01 +0800

    Site updated: 2022-05-12 12:31:00

--- dist/music.js
+++ dist/music.js
@@ -1,6 +1,16 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-	fixed: true,
+	mini: false,
+    autoplay: false,
+    theme: '#FADFA3',
+    loop: 'all',
+    order: 'random',
+    preload: 'auto',
+    volume: 0.7,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: 90,
+    lrcType: 3,
     audio: [
 		{
         name: '夜曲',
