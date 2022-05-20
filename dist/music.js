cb296e3a5264dd84e53185ca42acf6d00bcc4dce
yilong0110 <okyi@vip.qq.com>
2022-05-20 11:35:10 +0800

    Site updated: 2022-05-20 11:35:08

--- dist/music.js
+++ dist/music.js
@@ -31,5 +31,12 @@ const ap = new APlayer({
         cover: '/music/十一月的肖邦.jpg',
         lrc: '/music/枫.lrc',
     },
+    {
+        name: '危险派对',
+        artist: '王以太.刘至佳',
+        url: '/music/危险派对.flac',
+        cover: '/music/危险派对.jpg',
+        lrc: '/music/危险派对.lrc',
+    },
 	]
 });
